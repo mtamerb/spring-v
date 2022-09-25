1405ae1d8f674c1ed288cc9f9d5bb2ed4604c960
mtamerb <tamerbilici00@gmail.com>
2022-09-25 21:37:40 +0300

    Add spring libs and configuration context

--- src/com/tamerb/intro/Main.java
+++ src/com/tamerb/intro/Main.java
@@ -1,10 +1,20 @@
 package com.tamerb.intro;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //configuration context
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
         CustomerManager customerManager = new CustomerManager(new MySqlCustomerDal());
         customerManager.add();
+
+
     }
 }
 
